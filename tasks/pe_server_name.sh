3d364cf784fc8947299dd7a139c384893138f085
coffeeales <132638139+coffeeales@users.noreply.github.com>
2024-07-09 16:12:38 +1000

    Edited task: pe_server_name to get the name of the PE Server

--- tasks/pe_server_name.sh
+++ tasks/pe_server_name.sh
@@ -26,3 +26,21 @@
 #   }
 # Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
 #
+
+# Create output files
+output_file="pe_server_name_$(date +%F).txt"
+# html_output_file="tse-$(date +%F).html"
+create_date="$(date +%d\/%m\/%Y)"
+
+# Create a temporary directory
+# temp_dir=$(mktemp -d)
+# input_file="$temp_dir/cert-expire.txt"
+
+
+printf "$create_date" > "$output_file"
+printf "\n" >> "$output_file"
+printf "Collecting PE Server Name \n"
+printf "PE Server Name = " >> "$output_file"
+puppet config print | awk '/^server =/ {print $3}' >> "$output_file"
+
+
