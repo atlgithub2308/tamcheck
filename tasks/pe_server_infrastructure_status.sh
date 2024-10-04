df42adb5d2187c39e360485e6ee6f0d5c4a93909
TeckLeong <teckleong@-NWXW43QXK2-3.local>
2024-10-04 17:19:29 +0800

    added json output

--- tasks/pe_server_infrastructure_status.sh
+++ tasks/pe_server_infrastructure_status.sh
@@ -1,48 +1,18 @@
 #!/bin/sh
 
 # Puppet Task Name: pe_server_infrastructure_status
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
+# This script checks the Puppet Enterprise server infrastructure status,
+# writes the output to a file, and also converts it to a JSON format.
 
-if [ -d ${PT_output_dir} ]
-then
-    if [ ! -d "${PT_output_dir}/tamcheck_data" ]
-    then
+# Check if the output directory exists, create it if necessary
+if [ -d "${PT_output_dir}" ]; then
+    if [ ! -d "${PT_output_dir}/tamcheck_data" ]; then
         mkdir -p "${PT_output_dir}/tamcheck_data"
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
-    else
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
     fi
+    output_dir="${PT_output_dir}/tamcheck_data"
 else
     echo "No ${PT_output_dir} directory exists to dump files"
-    exit
+    exit 1
 fi
 
 # Ensure pathing is set to be able to run puppet commands
@@ -51,6 +21,23 @@ fi
 # File variable to use in redirections of command outputs to files
 output_file="${output_dir}/pe_server_infrastructure_status.out"
 
-puppet infrastructure status | tee $output_file
+# Generate the infrastructure status output and save to file
+puppet infrastructure status | tee "$output_file"
 
-echo " output_file is found here: ${output_file} "
+# Convert the output to JSON format
+json_output_file="${output_dir}/pe_server_infrastructure_status.json"
+
+# Transform the status file to JSON (basic transformation for illustrative purposes)
+awk '
+BEGIN { print "{\"infrastructure_status\": [" }
+{
+    gsub(/"/, "\\\"")  # Escape double quotes
+    if (NR>1) printf ",\n"
+    printf "  \"%s\"", $0
+}
+END { print "\n]}" }
+' "$output_file" > "$json_output_file"
+
+# Output the location of both the result files
+echo "Output file is found here: ${output_file}"
+echo "JSON output file is found here: ${json_output_file}"
