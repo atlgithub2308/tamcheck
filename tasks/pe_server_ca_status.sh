04b710e7012e6e6298c5fa817c9ece56e2f20937
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2024-10-30 12:59:07 +0800

    Update pe_server_ca_status.sh

--- tasks/pe_server_ca_status.sh
+++ tasks/pe_server_ca_status.sh
@@ -27,22 +27,19 @@
 # Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
 #
 
-if [ -d ${PT_output_dir} ]
-then
-    if [ ! -d "${PT_output_dir}/tamcheck_data" ]
-    then
+# Puppet Task Name: pe_server_ca_status
+# This script collects the Puppet Enterprise Server CA certificate expiry status,
+# saves it to a file, and also converts it to JSON format.
+
+# Check if the output directory exists, create it if necessary
+if [ -d "${PT_output_dir}" ]; then
+    if [ ! -d "${PT_output_dir}/tamcheck_data" ]; then
         mkdir -p "${PT_output_dir}/tamcheck_data"
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
-    else
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
     fi
+    output_dir="${PT_output_dir}/tamcheck_data"
 else
     echo "No ${PT_output_dir} directory exists to dump files"
-    exit
+    exit 1
 fi
 
 # Ensure pathing is set to be able to run puppet commands
@@ -50,14 +47,20 @@ fi
 
 # File variable to use in redirections of command outputs to files
 output_file="${output_dir}/pe_server_ca_status.out"
+json_output_file="${output_dir}/pe_server_ca_status.json"
 input_file="${output_dir}/pe_server_ca_status.in"
 
-# Puppet Server Cert Expiry
+# Collect the CA certificate expiry status and save it to the output file
 printf "Collecting PE Server CA Certificate Status \n"
-/opt/puppetlabs/puppet/bin/openssl x509 -in "$(/opt/puppetlabs/bin/puppet config print hostcert)" --enddate --noout > "$input_file"
+/opt/puppetlabs/puppet/bin/openssl x509 -in "$(/opt/puppetlabs/bin/puppet config print hostcert)" -enddate -noout > "$input_file"
 awk '{print "Puppet Enterprise Server Certificate valid until: " substr($1,10,3) " " $2 " " $4}' "$input_file" | tee "$output_file"
 rm -f "$input_file"
 
+# Convert the output file to JSON format
+expiry_date=$(awk '{print $7, $8, $9}' "$output_file")
+echo "{ \"PEServerCACertificateExpiry\": \"$expiry_date\" }" > "$json_output_file"
+
 echo ""
-echo "Output_file is found here: ${output_file} "
+echo "Output file is found here: ${output_file}"
+echo "JSON output file is found here: ${json_output_file}"
 echo ""
