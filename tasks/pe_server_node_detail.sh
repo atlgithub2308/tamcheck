db6b133066e72d186b1f85100b984b563f722df3
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2024-11-04 13:28:27 +0800

    Update pe_server_node_detail.sh

--- tasks/pe_server_node_detail.sh
+++ tasks/pe_server_node_detail.sh
@@ -1,72 +1,70 @@
 #!/bin/sh
 
 # Puppet Task Name: pe_server_node_detail
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
-if [ -d ${T_output_dir} ]
-then
-    if [ ! -d "${PT_output_dir}/tamcheck_data" ]
-    then
+
+if [ -d "${PT_output_dir}" ]; then
+    if [ ! -d "${PT_output_dir}/tamcheck_data" ]; then
         mkdir -p "${PT_output_dir}/tamcheck_data"
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
-    else
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
     fi
+    output_dir="${PT_output_dir}/tamcheck_data"
 else
     echo "No ${PT_output_dir} directory exists to dump files"
-    exit
+    exit 1
 fi
 
 # Ensure pathing is set to be able to run puppet commands
 [[ $PATH =~ "/opt/puppetlabs/bin" ]] || export PATH="/opt/puppetlabs/bin:${PATH}"
 
 # File variable to use in redirections of command outputs to files
-output_file_pe_node_count="${output_dir}/pe_node_count.out"
+output_file_pe_node_count="${output_dir}/pe_server_node_detail.out"
+json_output_file="${output_dir}/pe_server_node_detail.json"
 
-echo "" > $output_file_pe_node_count
-echo -n "PE Server Total Node Count: " >> $output_file_pe_node_count
-puppet query 'nodes[count(certname)]{}' | awk '/"count":/ {print $2}' >> $output_file_pe_node_count
-echo "" >> $output_file_pe_node_count
+# Initialize output files
+echo "" > "$output_file_pe_node_count"
+declare -A node_counts
 
-echo -n "PE Server Node Count (minus de-actived & expired nodes): " >> $output_file_pe_node_count
-puppet query 'nodes[count(certname)]{deactivated is null and expired is null}' | awk '/"count":/ {print $2}' >> $output_file_pe_node_count
-echo "" >> $output_file_pe_node_count
+# Get total PE Server Node Count
+node_counts[total]=$(puppet query 'nodes[count(certname)]{}' | awk '/"count":/ {print $2}')
+echo -n "PE Server Total Node Count: ${node_counts[total]}" >> "$output_file_pe_node_count"
+echo "" >> "$output_file_pe_node_count"
 
-echo -n "PE Server Node Count (Number of Nodes not expired): " >> $output_file_pe_node_count
-puppet query 'nodes[count(certname)]{expired is null}' | awk '/"count":/ {print $2}' >> $output_file_pe_node_count 
-echo "" >> $output_file_pe_node_count
+# Get PE Server Node Count (minus deactivated & expired nodes)
+node_counts[active]=$(puppet query 'nodes[count(certname)]{deactivated is null and expired is null}' | awk '/"count":/ {print $2}')
+echo -n "PE Server Node Count (minus de-activated & expired nodes): ${node_counts[active]}" >> "$output_file_pe_node_count"
+echo "" >> "$output_file_pe_node_count"
 
-echo -n "PE Server Node Count (Inactive nodes): " >> $output_file_pe_node_count
-puppet query 'nodes[count(certname)]{node_state = "inactive"}' | awk '/"count":/ {print $2}' >> $output_file_pe_node_count
-echo "" >> $output_file_pe_node_count
+# Get PE Server Node Count (Number of Nodes not expired)
+node_counts[not_expired]=$(puppet query 'nodes[count(certname)]{expired is null}' | awk '/"count":/ {print $2}')
+echo -n "PE Server Node Count (Number of Nodes not expired): ${node_counts[not_expired]}" >> "$output_file_pe_node_count"
+echo "" >> "$output_file_pe_node_count"
 
-echo -n "PE Server Node Count (Nodes using a cached catalog): " >> $output_file_pe_node_count
-puppet query 'nodes[count(certname)]{cached_catalog_status = "used"}' | awk '/"count":/ {print $2}' >> $output_file_pe_node_count
-echo "" >> $output_file_pe_node_count
+# Get PE Server Node Count (Inactive nodes)
+node_counts[inactive]=$(puppet query 'nodes[count(certname)]{node_state = "inactive"}' | awk '/"count":/ {print $2}')
+echo -n "PE Server Node Count (Inactive nodes): ${node_counts[inactive]}" >> "$output_file_pe_node_count"
+echo "" >> "$output_file_pe_node_count"
+
+# Get PE Server Node Count (Nodes using a cached catalog)
+node_counts[cached]=$(puppet query 'nodes[count(certname)]{cached_catalog_status = "used"}' | awk '/"count":/ {print $2}')
+echo -n "PE Server Node Count (Nodes using a cached catalog): ${node_counts[cached]}" >> "$output_file_pe_node_count"
+echo "" >> "$output_file_pe_node_count"
+
+# Create JSON output
+jq -n \
+    --arg total "${node_counts[total]}" \
+    --arg active "${node_counts[active]}" \
+    --arg not_expired "${node_counts[not_expired]}" \
+    --arg inactive "${node_counts[inactive]}" \
+    --arg cached "${node_counts[cached]}" \
+    '{
+        "PE_Server_Total_Node_Count": $total,
+        "PE_Server_Node_Count_Active": $active,
+        "PE_Server_Node_Count_Not_Expired": $not_expired,
+        "PE_Server_Node_Count_Inactive": $inactive,
+        "PE_Server_Node_Count_Cached": $cached
+    }' > "$json_output_file"
+
+echo ""
+echo "Output files are located at:"
+echo "Text output: ${output_file_pe_node_count}"
+echo "JSON output: ${json_output_file}"
+echo ""
