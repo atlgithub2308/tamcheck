86d06ab4e4b3b4954dea640098c9b4e1d1f61dec
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2024-11-04 10:46:57 +0800

    Update pe_server_node_count.sh

--- tasks/pe_server_node_count.sh
+++ tasks/pe_server_node_count.sh
@@ -1,60 +1,38 @@
 #!/bin/sh
 
-# Puppet Task Name: pe_server_node_count
+# Puppet Task Name: pe_server_node_count_with_json
 #
-# This is where you put the shell code for your task.
+# This script collects the Puppet Enterprise server node count and saves the output
+# both in a standard text file and as a JSON file.
 #
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
-
-if [ -d ${PT_output_dir} ]
-then
-    if [ ! -d "${PT_output_dir}/tamcheck_data" ]
-    then
+# Ensure the output directory exists or exit with an error
+if [ -d "${PT_output_dir}" ]; then
+    if [ ! -d "${PT_output_dir}/tamcheck_data" ]; then
         mkdir -p "${PT_output_dir}/tamcheck_data"
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
-    else
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
     fi
+    output_dir="${PT_output_dir}/tamcheck_data"
 else
     echo "No ${PT_output_dir} directory exists to dump files"
-    exit
+    exit 1
 fi
 
-# Ensure pathing is set to be able to run puppet commands
+# Ensure Puppet commands can be found in the PATH
 [[ $PATH =~ "/opt/puppetlabs/bin" ]] || export PATH="/opt/puppetlabs/bin:${PATH}"
 
-# File variable to use in redirections of command outputs to files
+# File paths for output
 output_file="${output_dir}/pe_server_node_count.out"
-# Get PE Server node count
+json_output_file="${output_dir}/pe_server_node_count.json"
+
+# Get PE Server node count and write to text and JSON files
 printf "Collecting PE Server Node Count \n"
 printf "Number of Nodes = " | tee "$output_file"
-puppet query 'nodes[count(certname)]{deactivated is null and expired is null}' | awk '/    "count":/ {print $2}' | tee -a "$output_file"
+node_count=$(puppet query 'nodes[count(certname)]{deactivated is null and expired is null}' | awk '/    "count":/ {print $2}')
+echo "$node_count" | tee -a "$output_file"
+
+# Create JSON output
+echo "{\"node_count\": $node_count}" > "$json_output_file"
 
 echo ""
-echo "Output_file is found here: ${output_file} "
+echo "Output_file is found here: ${output_file}"
+echo "JSON output file is found here: ${json_output_file}"
 echo ""
