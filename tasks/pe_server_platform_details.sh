79ef190d89fbed0147bb624517abee527c437e46
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2024-11-05 14:25:22 +0800

    Update pe_server_platform_details.sh

--- tasks/pe_server_platform_details.sh
+++ tasks/pe_server_platform_details.sh
@@ -1,99 +1,59 @@
 #!/bin/sh
 
 # Puppet Task Name: pe_server_platform_details
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
-#!/bin/sh
 
-# Puppet Task Name: pe_server_node_detail
-#
-# This is where you put the shell code for your task.
-#
-# You can write Puppet tasks in any language you want and it's easy to
-# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# https://puppet.com/docs/bolt/0.x/writing_tasks.html
-#
-# Puppet tasks make it easy for you to enable others to use your script. Tasks
-# describe what it does, explains parameters and which are required or optional,
-# as well as validates parameter type. For examples, if parameter "instances"
-# must be an integer and the optional "datacenter" parameter must be one of
-# portland, sydney, belfast or singapore then the .json file
-# would include:
-#   "parameters": {
-#     "instances": {
-#       "description": "Number of instances to create",
-#       "type": "Integer"
-#     },
-#     "datacenter": {
-#       "description": "Datacenter where instances will be created",
-#       "type": "Enum[portland, sydney, belfast, singapore]"
-#     }
-#   }
-# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
-#
-if [ -d ${PT_output_dir} ]
-then
-    if [ ! -d "${PT_output_dir}/tamcheck_data" ]
-    then
+# Check for the output directory
+if [ -d "${PT_output_dir}" ]; then
+    # Create output directory if it doesn't exist
+    if [ ! -d "${PT_output_dir}/tamcheck_data" ]; then
         mkdir -p "${PT_output_dir}/tamcheck_data"
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
-    else
-        output_dir="${PT_output_dir}"
-        output_dir+="/"
-        output_dir+="tamcheck_data"
     fi
+    output_dir="${PT_output_dir}/tamcheck_data"
 else
     echo "No ${PT_output_dir} directory exists to dump files"
-    exit
+    exit 1
 fi
 
 # Ensure pathing is set to be able to run puppet commands
 [[ $PATH =~ "/opt/puppetlabs/bin" ]] || export PATH="/opt/puppetlabs/bin:${PATH}"
 
-# File variable to use in redirections of command outputs to files
-output_file="${output_dir}/pe_server_platform_detail.out"
+# Define output files
+output_file="${output_dir}/pe_server_platform_details.out"
+json_output_file="${output_dir}/pe_server_platform_details.json"
 
-fqdn=`puppet facts networking.fqdn --render-as s | awk -F\" {'print $4}'`
-ip=`puppet facts networking.ip --render-as s | awk -F\" '{print $4}'`
-os_name=`puppet facts os.name --render-as s | awk -F\" '{print $4}'`
-os_ver=`puppet facts os.release.full --render-as s | awk -F\" '{print $4}'`
-vcpu=`puppet facts processors.count --render-as s | awk -F\> '{print $2}' | sed 's/\}//g'`
-cpu_model=`puppet facts processors.models --render-as s | awk -F\" '{print $4}'`
-memory=`puppet facts memory.system.total --render-as s | awk -F\" '{print $4}'`
+# Collect platform details using Puppet facts
+fqdn=$(puppet facts networking.fqdn --render-as s | awk -F\" '{print $4}')
+ip=$(puppet facts networking.ip --render-as s | awk -F\" '{print $4}')
+os_name=$(puppet facts os.name --render-as s | awk -F\" '{print $4}')
+os_ver=$(puppet facts os.release.full --render-as s | awk -F\" '{print $4}')
+vcpu=$(puppet facts processors.count --render-as s | awk -F\> '{print $2}' | sed 's/\}//g')
+cpu_model=$(puppet facts processors.models --render-as s | awk -F\" '{print $4}')
+memory=$(puppet facts memory.system.total --render-as s | awk -F\" '{print $4}')
 
-echo "PE Server FQDN: ${fqdn}" | tee $output_file
-echo "PE Server ip address: ${ip}" | tee -a $output_file
-echo "PE Server operating system: ${os_name}" | tee -a $output_file
-echo "PE Server operating system version: ${os_ver}" | tee -a $output_file
-echo "PE Server vCPU count: ${vcpu}" | tee -a $output_file
-echo "PE Server CPU type: ${cpu_model}" | tee -a $output_file
-echo "PE Server Memory: ${memory}" | tee -a $output_file
+# Output to console and .out file
+echo "PE Server FQDN: ${fqdn}" | tee "$output_file"
+echo "PE Server IP Address: ${ip}" | tee -a "$output_file"
+echo "PE Server Operating System: ${os_name}" | tee -a "$output_file"
+echo "PE Server Operating System Version: ${os_ver}" | tee -a "$output_file"
+echo "PE Server vCPU Count: ${vcpu}" | tee -a "$output_file"
+echo "PE Server CPU Model: ${cpu_model}" | tee -a "$output_file"
+echo "PE Server Memory: ${memory}" | tee -a "$output_file"
+
+# Create JSON output file
+cat <<EOF > "$json_output_file"
+{
+    "PE Server FQDN": "$fqdn",
+    "PE Server IP Address": "$ip",
+    "PE Server Operating System": "$os_name",
+    "PE Server Operating System Version": "$os_ver",
+    "PE Server vCPU Count": "$vcpu",
+    "PE Server CPU Model": "$cpu_model",
+    "PE Server Memory": "$memory"
+}
+EOF
 
 echo ""
-echo "Output_file is found here: ${output_file} "
+echo "Output files are located at:"
+echo "Text output: ${output_file}"
+echo "JSON output: ${json_output_file}"
 echo ""
